18f1c00c1bf2cdac9df062fc4ee0e5d8cde7ab35
Christhian Montes Aguilar <39229428+stevenmontes@users.noreply.github.com>
2021-09-26 20:36:37 -0600

    Agrega home y cambio de icono

--- app/src/main/java/com/example/loginfirebase/AuthActivity.kt
+++ app/src/main/java/com/example/loginfirebase/AuthActivity.kt
@@ -1,9 +1,11 @@
 package com.example.loginfirebase
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.CheckBox
 import android.widget.EditText
 import androidx.appcompat.app.AlertDialog
 import com.google.firebase.auth.FirebaseAuth
@@ -11,13 +13,30 @@ import com.google.firebase.auth.FirebaseAuth
 class AuthActivity : AppCompatActivity() {
     private lateinit var emailEditText : EditText
     private lateinit var passwordEditText: EditText
+    private lateinit var logInCheckBox: CheckBox
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_auth)
+        setUp()
+        session()
+    }
+
+    private fun setUp(){
         title = "Autenticación"
         emailEditText = findViewById(R.id.emailEditText)
         passwordEditText = findViewById(R.id.passwordEditText)
+        logInCheckBox = findViewById(R.id.logInCheckBox)
+    }
+
+    private fun session(){
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE)
+        val email = prefs.getString("email", null)
+
+        if (email != null){
+            showHome(email)
+        }
+
     }
 
     fun singUp(view: View){
@@ -25,7 +44,7 @@ class AuthActivity : AppCompatActivity() {
             FirebaseAuth.getInstance().createUserWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString())
                 .addOnCompleteListener{
                     if (it.isSuccessful){
-                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        showHome(it.result?.user?.email ?: "")
                     } else {
                         showAlert("Error", it.exception?.message ?: "")
                     }
@@ -37,11 +56,14 @@ class AuthActivity : AppCompatActivity() {
             FirebaseAuth.getInstance().signInWithEmailAndPassword(emailEditText.text.toString(), passwordEditText.text.toString())
                 .addOnCompleteListener{
                     if (it.isSuccessful){
-                        showHome(it.result?.user?.email ?: "", ProviderType.BASIC)
+                        showHome(it.result?.user?.email ?: "")
                     } else {
                         showAlert("Error", "Se ha producido un error autenticando el usuario")
                     }
                 }
+        }
+        else{
+            showAlert("Cuidado", "Ingrese su correo y contraseña")
         }
     }
 
@@ -54,10 +76,10 @@ class AuthActivity : AppCompatActivity() {
         dialog.show()
     }
 
-    private fun showHome(email: String, providerType: ProviderType){
+    private fun showHome(email: String){
         val homeIntent = Intent(this, HomeActivity::class.java).apply {
             putExtra("email", email)
-            putExtra("provider", providerType.name)
+            putExtra("autoLogIn", logInCheckBox.isChecked)
         }
         startActivity(homeIntent)
     }
--- app/src/main/java/com/example/loginfirebase/HomeActivity.kt
+++ app/src/main/java/com/example/loginfirebase/HomeActivity.kt
@@ -1,15 +1,67 @@
 package com.example.loginfirebase
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
-
-enum class ProviderType{
-    BASIC
-}
+import android.view.View
+import android.widget.TextView
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.loginfirebase.databinding.ActivityHomeBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+    private lateinit var currentEmailTextView: TextView
+    private lateinit var currentAutoLoginTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_home)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+        val bundle = intent.extras
+        val email = bundle?.getString("email")
+        val autoLogin = bundle?.getBoolean("autoLogIn")
+
+        if (email != null) {
+            setUp(email)
+
+            if (autoLogin == true){
+                val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+                prefs.putString("email", email)
+                prefs.apply()
+            }
+        }
+    }
+
+    private fun setUp(email: String){
+        currentEmailTextView = findViewById((R.id.currentEmailTextView))
+        currentEmailTextView.text = email
+    }
+
+    fun logOut(view: View){
+        val prefs = getSharedPreferences(getString(R.string.prefs_file), Context.MODE_PRIVATE).edit()
+        prefs.clear()
+        prefs.apply()
+        FirebaseAuth.getInstance().signOut()
+        onBackPressed()
     }
 }
