c0ead225d1f59fbc518aa69b645fe0742e4b34f3
Rafa1390 <32964996+Rafa1390@users.noreply.github.com>
2021-10-21 17:28:57 -0600

    Se listan todos los empleados

--- app/src/main/java/com/example/loginfirebase/EmployeesListActivity.kt
+++ app/src/main/java/com/example/loginfirebase/EmployeesListActivity.kt
@@ -2,23 +2,31 @@ package com.example.loginfirebase
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.loginfirebase.models.Employee
 import com.example.loginfirebase.viewmodel.FirestoreViewEmployeeModel
 
 class EmployeesListActivity : AppCompatActivity() {
-    private lateinit var viewModel: FirestoreViewEmployeeModel
+    private lateinit var adapter: MainAdapter
+    private val viewModel by lazy { ViewModelProvider(this)[FirestoreViewEmployeeModel::class.java] }
+
+    private lateinit var employeesListRV: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_employees_list)
 
-        viewModel = ViewModelProvider(this)[FirestoreViewEmployeeModel::class.java]
-
         //Establece los elementos de la vista
         setUp(intent.extras?.getString("email"))
 
-        //Se listan los empleados
-        //getEmployeesList()
+        adapter = MainAdapter(this)
+        employeesListRV = findViewById(R.id.employeesListRV)
+        employeesListRV.layoutManager = LinearLayoutManager(this)
+        employeesListRV.adapter = adapter
+        observeData()
     }
 
     private fun setUp(email: String?) {
@@ -30,7 +38,10 @@ class EmployeesListActivity : AppCompatActivity() {
 //        userLogEmailTextView.text = prayer.toString()
     }
 
-    private fun getEmployeesList() {
-        viewModel.getAllEmployeesVM()
+    fun observeData() {
+        viewModel.fetchEmployeesData().observe(this, Observer {
+            adapter.setListData(it)
+            adapter.notifyDataSetChanged()
+        })
     }
 }
--- app/src/main/java/com/example/loginfirebase/viewmodel/FirestoreViewEmployeeModel.kt
+++ app/src/main/java/com/example/loginfirebase/viewmodel/FirestoreViewEmployeeModel.kt
@@ -1,11 +1,14 @@
 package com.example.loginfirebase.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.loginfirebase.domain.FirestoreEmployeeStory
+import com.example.loginfirebase.data.repo.FirebaseEmployeeRepo
 import com.example.loginfirebase.models.Employee
 
 class FirestoreViewEmployeeModel: ViewModel() {
-    private val firestoreEmployeeStory = FirestoreEmployeeStory()
+    //private val firestoreEmployeeStory = FirestoreEmployeeStory()
+    private val repo = FirebaseEmployeeRepo()
     private val employee = Employee()
 
     fun createEmployee(employerEmail: String?, id: String, name: String, email: String, job: String, department: String) {
@@ -16,11 +19,15 @@ class FirestoreViewEmployeeModel: ViewModel() {
             employee.email = email
             employee.job = job
             employee.department = department
-            firestoreEmployeeStory.setEmployeeFirestore(employee)
+            repo.setEmployeeData(employee)
         }
     }
 
-    fun getAllEmployeesVM(){
-        firestoreEmployeeStory.getAllEmployees()
+    fun fetchEmployeesData(): LiveData<MutableList<Employee>>{
+        val mutableData = MutableLiveData<MutableList<Employee>>()
+        repo.getAllEmployeesData().observeForever { employeeList ->
+            mutableData.value = employeeList
+        }
+        return mutableData
     }
 }
